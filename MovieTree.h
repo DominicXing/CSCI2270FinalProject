a5625fe4772018e7d2686da69c55fb7906eeecd7
DominicXing <goxi3959@gmail.com>
2015-04-27 23:02:14 -0600

    Update MovieTree.h

--- MovieTree.h
+++ MovieTree.h
@@ -1,43 +1,58 @@
 #ifndef MOVIETREE_H
 #define MOVIETREE_H
 
+#include<string>
 
-struct MovieNode{
+struct MovieNode
+{
+  MovieNode();
+  MovieNode(int& rank, std::string& ttl, int& yr, int& qtty);
+  virtual ~MovieNode();
+  friend std::ostream& operator<<(std::ostream& os, MovieNode* n);
+
+  MovieNode* parent;
+  MovieNode* left;
+  MovieNode* right;
     int ranking;
     std::string title;
     int year;
     int quantity;
-    MovieNode *parent;
-    MovieNode *leftChild;
-    MovieNode *rightChild;
-
-    MovieNode(){};
-
-    MovieNode(int in_ranking, std::string in_title, int in_year, int in_quantity)
-    {
-        ranking = in_ranking;
-        title = in_title;
-        year = in_year;
-        quantity = in_quantity;
-    }
-
 };
+
 
 class MovieTree
 {
-    public:
-        MovieTree();
-        virtual ~MovieTree();
-        void printMovieInventory();
-        void addMovieNode(int ranking, std::string title, int releaseYear, int quantity);
-        void findMovie(std::string title);
-        void rentMovie(std::string title);
-    protected:
-    private:
-        void DeleteAll(MovieNode * node);
-        void printMovieInventory(MovieNode * node);
-        MovieNode* searchMovieTree(MovieNode * node, std::string title);
-        MovieNode *root;
+public:
+  MovieTree();
+  virtual ~MovieTree();
+
+  inline void test()
+  {
+    delete_tree(root);
+  }
+
+  void addRawNode(int&, std::string&, int&, int&);
+  void insert(MovieNode*);        //adds a new node
+  void insert(MovieNode*, MovieNode*);  //adds a new node to a specified sub tree
+
+  void inorder_walk();          //external call to root
+  MovieNode* search(std::string&);    //finds node with the passed key
+  MovieNode* iterative_search(std::string&);  //while version of search, non-recursive
+  MovieNode* minimum();         //find min (up for debate wether or not these should be private)
+  MovieNode* maximum();         //find max (^*************************************************^)
+
+private:
+  MovieNode *root;
+
+  void inorder_walk(MovieNode*);      //internal reccursive call
+  MovieNode* search(MovieNode*, std::string&);//recursive, and sub-tree version of search 
+  MovieNode* minimum(MovieNode*);     //find min of specified sub-tree
+  MovieNode* maximum(MovieNode*);     //find max of specified sub-node
+  MovieNode* successor(MovieNode*);   //next incriment
+  MovieNode* predecessor(MovieNode*);   //next smaller
+  void transplant(MovieNode*, MovieNode*);//switches dangling trees on a parent
+  void delete_node(MovieNode*);     //removes a node
+  void delete_tree(MovieNode*);     //delete whole tree. Not purely in ~MovieTree() because it was easier to debugg.
 };
 
 #endif // MOVIETREE_H
